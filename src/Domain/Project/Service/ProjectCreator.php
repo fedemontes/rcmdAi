413a936da62db3f59805fe41ea4c4a7dc71f2a4c
fedemontes <frederic.montes@gmail.com>
2023-11-27 20:57:26 +0100

    improve 1.x

--- src/Domain/Project/Service/ProjectCreator.php
+++ src/Domain/Project/Service/ProjectCreator.php
@@ -69,11 +69,11 @@ final class ProjectCreator
         El objetivo del proyecto es $proyecto_obj.  
         La empresa no ha hecho nada similar a esto antes. La empresa quiere utilizar $tecnologias.
         Además también usar $sostenibilidad. Quiero que definas con contenido específico las fases del proyecto.
-        Quiero una planificación aproximada. Quiero una orientación de KPI para evaluar el impacto del mismo. 
+        Necesito una planificación aproximada. Proporcióname una orientación de KPI para evaluar el impacto del proyecto. 
         Pónmelo todo en un lenguaje cercano y motivador para un público joven.  Índícame marcas de referencia de la
         competencia que hay a nivel europeo y su link a la web. Si hay presencia de redes sociales, por favor, 
         indícame qué hashtags consideras más adecuados. 
-        Recuerda que es un trabajo final de grado y que esto lo hará un estudiante.
+        Recuerda que es un $proyecto y que esto lo hará un estudiante.
         OPENAI_CONTENT;
 
         $response = $client->chat()->create([
