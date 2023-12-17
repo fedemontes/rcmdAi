bd49c49029dc7c79172a95255586599d0c6091cf
fedemontes <frederic.montes@gmail.com>
2023-12-17 12:51:41 +0100

    OPENAI warnings

--- src/Domain/Project/Service/ProjectCreator.php
+++ src/Domain/Project/Service/ProjectCreator.php
@@ -64,7 +64,7 @@ final class ProjectCreator
         $proyecto_objetivo  = isset ($data['proyecto_objetivo']) ? $data['proyecto_objetivo'] : $vision;
         $sostenibilidad     = isset ($data['sostenibilidad']) ? $data['sostenibilidad'] : $sector;
         $tecnologias        = isset ($data['tecnologias']) ? $data['tecnologias'] : "tecnologías emergentes";
-        $experiencia        = isset ($data['experiencia'] ) ? $data['eperiencia'] : "no";
+        $experiencia        = $data['experiencia'] == true ? "sí" : "no";
 
         $content = <<<OPENAI_CONTENT
         Actúa como un especialista en $perfil_ideal que lleva  20 años trabajando. 
